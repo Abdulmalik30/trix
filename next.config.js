b9a1915d32295ddfca9151dc2c77027fbeeaa583
Abiola <abdulmalikmuhiz01@gmail.com>
2023-05-12 16:14:59 +0100

    updated file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  basePath: '',
+  i18n: {
+    locales: ['en-US', 'fr', 'es'],
+    defaultLocale: 'en-US',
+  },
+  images: {
+    domains: ['example.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Origin, X-Requested-With, Content-Type, Accept',
+          },
+        ],
+      },
+    ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Custom webpack configuration options
+    return config;
+  },
+};
